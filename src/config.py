8b000cc8b3ceaebd71ad4d10d042242efaf1a0bd
annec22 <annecalija@gmail.com>
2017-10-31 11:29:11 +0800

    update gitignore for exclusion of pycharm ag files, update config.py, and requirements

---
+++ src/config.py
@@ -0,0 +1,14 @@
+import os
+
+# The secret key is used by Flask to encrypt session cookies.
+SECRET_KEY = 'secret'
+
+DATA_BACKEND = 'datastore'
+
+PROJECT_ID = 'simplejournal-184519'
+
+# Mongo configuration
+# If using mongolab, the connection URI is available from the mongolab control
+# panel. If self-hosting on compute engine, replace the values below.
+MONGO_URI = \
+    'mongodb://user:password@host:27017/database'
